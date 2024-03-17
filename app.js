0dfbb9c62fc0ad2ad26c99c51e56a82f9139d1ce
Ashok Kuduva Ananthan <kaashok1993@gmail.com>
2024-03-17 17:40:02 +0530

    github server

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv=require('dotenv').config({path: './.env'});
+const mod = require('./modules').module;
+var app = mod.express();
 
-const app = express();
-const PORT=8080;
+// view engine setup
+app.set('views', mod.path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(express.json());
-app.use(cors());
+app.use(mod.logger('dev'));
+app.use(mod.express.json());
+app.use(mod.express.urlencoded({ extended: false }));
+app.use(mod.cookieParser());
+app.use(mod.express.static(mod.path.join(__dirname, 'public')));
 
-app.get('/', (req,res)=>{
-    res.send(`Welcome ${process.env.GITHUB_ACCESS_TOKEN} to Github NodeJS API app!`)
+app.use(mod.cors({
+  origin: ['http://localhost:4200'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE']
+}))
+
+app.use(mod.cookieSession({
+  name: 'sess', //name of the cookie in the browser
+  secret: 'asdfgh',
+  httpOnly: true
+}))
+const indexRouter = require('./routes/index');
+
+
+app.use('/oauth', indexRouter);
+
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status).send();
+});
+
+app.listen(mod.config.APP_PORT, function () {
+  console.log("app listening on port" + mod.config.APP_PORT);
 })
 
-app.listen(PORT,()=>console.log(`Server started on port ${PORT}...`))
+module.exports = app;
